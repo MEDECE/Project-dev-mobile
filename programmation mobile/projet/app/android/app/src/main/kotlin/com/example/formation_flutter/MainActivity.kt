4fb8e8532ce95fc59a16744f181ee31fe9bac210
MEDECE <131447534+MEDECE@users.noreply.github.com>
2026-02-23 18:43:15 +0100

    Delete programmation mobile directory

--- programmation mobile/projet/app/android/app/src/main/kotlin/com/example/formation_flutter/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.formation_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- programmation mobile/projet/app/lib/res/app_vectorial_images.dart
+++
@@ -1,6 +0,0 @@
-class AppVectorialImages {
-  AppVectorialImages._();
-
-  static const String appLogo = 'res/svg/app_logo.svg';
-  static const String illEmpty = 'res/svg/ill_empty.svg';
-}
